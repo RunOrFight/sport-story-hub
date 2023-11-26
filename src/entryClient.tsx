0d97d459d793bf6b4d565e60d242a26798027f97
RunOrFight <dragoinfury@gmail.com>
2023-11-26 10:37:07 +0300

    feat: add netlify.toml

--- src/entryClient.tsx
+++ src/entryClient.tsx
@@ -3,7 +3,6 @@ import {BrowserRouter} from "react-router-dom"
 import {App} from "./App";
 import {Provider} from "react-redux";
 import {configureStore} from "@reduxjs/toolkit";
-import "../dist/antd.min.css"
 
 const data = window.__SSR_DATA__
 delete window.__SSR_DATA__
