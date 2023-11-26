8891a0f435630a86fc604755737e3b6060dbbe06
RunOrFight <dragoinfury@gmail.com>
2023-11-26 11:37:19 +0300

    refactor: prepare app to netlify

--- src/server.ts
+++ src/server.ts
@@ -7,96 +7,98 @@ import redux from "@reduxjs/toolkit";
 import {routeMap} from "./routeMap";
 import {config} from "dotenv";
 import {extractStyle} from '@ant-design/static-style-extract';
+import serverless from "serverless-http";
 
 config()
 
-const initApp = async () => {
-    const style = extractStyle()
+const style = extractStyle()
 
-    const app = express()
-    app.use(express.json())
+const app = express()
+app.use(express.json())
 
-    let devServer: any;
+let devServer: any;
 
-    if (process.env.APP_MODE === "DEV") {
-        devServer = await (
-            await import("vite")
-        ).createServer({
-            server: {middlewareMode: true},
-            appType: "custom",
-        })
-        app.use(devServer.middlewares)
-    } else {
-        app.use((await import("compression")).default())
-        app.use(
-            (await import("serve-static")).default(path.resolve("dist/client"), {
-                index: false,
-            }),
-        )
-    }
-
-
-    app.use(routeMap.apiCreateEventRoute, (req, res) => {
-        createFullEvent(req.body)
-
-        res.sendStatus(200)
+if (process.env.APP_MODE === "DEV") {
+    devServer = await (
+        await import("vite")
+    ).createServer({
+        server: {middlewareMode: true},
+        appType: "custom",
     })
+    app.use(devServer.middlewares)
+} else {
+    app.use((await import("compression")).default())
+    app.use(
+        (await import("serve-static")).default(path.resolve("dist/client"), {
+            index: false,
+        }),
+    )
+}
 
-    app.use("/favicon.ico", (_req, res) => {
+app.use(routeMap.apiCreateEventRoute, (req, res) => {
+    createFullEvent(req.body)
 
-        res.sendStatus(200)
-    })
+    res.sendStatus(200)
+})
 
-    app.use("*", async (req, res, next) => {
-        const route = req.originalUrl
-        let template, render
+app.use("/favicon.ico", (_req, res) => {
 
-        try {
-            if (process.env.APP_MODE === "DEV") {
-                template = fs.readFileSync(path.resolve("./index.html"), "utf-8")
+    res.sendStatus(200)
+})
 
-                template = await devServer.transformIndexHtml(route, template)
+app.use('/.netlify/functions/server/*', async (req, res, next) => {
+    const route = req.originalUrl
+    let template, render
 
-                render = (await devServer.ssrLoadModule("src/entryServer.tsx")).render
-            } else {
-                template = fs.readFileSync(
-                    path.resolve("dist/client/index.html"),
-                    "utf-8"
-                )
-                // @ts-ignore
-                render = (await import("../dist/server/entryServer.js")).render
-            }
+    try {
+        if (process.env.APP_MODE === "DEV") {
+            template = fs.readFileSync(path.resolve("./index.html"), "utf-8")
 
-            const data = getDataByRoute(route)
+            template = await devServer.transformIndexHtml(route, template)
 
-            const store = redux.configureStore({reducer: state => state, preloadedState: data})
-
-            const appHtml = await render({route, store})
-
-            const dataString = `<script>window.__SSR_DATA__=${JSON.stringify(data)}</script>`
-
-            const styleString = `<style>${style}</style>`
-
-            const html = template
-                .replace(`<!--style-outlet-->`, styleString)
-                .replace(`<!--ssr-outlet-->`, appHtml)
-                .replace(`<!--ssr-data-->`, dataString)
-
-            res.status(200).set({"Content-Type": "text/html"}).end(html)
-        } catch (error) {
-
-            if (process.env.APP_MODE === "DEV" && error instanceof Error) {
-                devServer.ssrFixStacktrace(error)
-            }
-            next(error)
+            render = (await devServer.ssrLoadModule("src/entryServer.tsx")).render
+        } else {
+            template = fs.readFileSync(
+                path.resolve("dist/client/index.html"),
+                "utf-8"
+            )
+            // @ts-ignore
+            render = (await import("../dist/server/entryServer.js")).render
         }
-    })
 
-    const expressAppPort = process.env.APP_PORT!
+        const data = getDataByRoute(route)
 
+        const store = redux.configureStore({reducer: state => state, preloadedState: data})
+
+        const appHtml = await render({route, store})
+
+        const dataString = `<script>window.__SSR_DATA__=${JSON.stringify(data)}</script>`
+
+        const styleString = `<style>${style}</style>`
+
+        const html = template
+            .replace(`<!--style-outlet-->`, styleString)
+            .replace(`<!--ssr-outlet-->`, appHtml)
+            .replace(`<!--ssr-data-->`, dataString)
+
+        res.status(200).set({"Content-Type": "text/html"}).end(html)
+    } catch (error) {
+
+        if (process.env.APP_MODE === "DEV" && error instanceof Error) {
+            devServer.ssrFixStacktrace(error)
+        }
+        next(error)
+    }
+})
+
+const expressAppPort = process.env.APP_PORT!
+
+
+if (process.env.APP_MODE === "DEV") {
     app.listen(expressAppPort, () => {
         console.log(chalk.bgBlue("EXPRESS STARTED"), chalk.blue(`port -> "${expressAppPort}"`))
     })
 }
 
-initApp()
+module.exports = app;
+module.exports.handler = serverless(app);
